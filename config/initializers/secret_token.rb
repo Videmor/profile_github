70f73a4bde321d0af171bb38e3994598df1ef9fc
videmor <videmor@gmail.com>
2014-01-09 05:54:42 -0500

    :octocat: first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Profile::Application.config.secret_key_base = 'cc9c8718ae65b91a557b44366dd4fccfe75f36b1a6c9f03436184925bd2d7bdebf8754ef474ab3ad30210114e0c4d7e5b87f7a22074be007fe211576506fc38b'
